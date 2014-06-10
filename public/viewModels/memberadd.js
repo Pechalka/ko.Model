df8be43f617ddf1a404cffda2c7d8794d2d35a25
pechalka <luzkovskyandrey@gmail.com>
2014-06-10 20:23:29 +0300

    sdf

--- public/viewModels/memberadd.js
+++ public/viewModels/memberadd.js
@@ -36,47 +36,6 @@ var Member = ko.Model({
 
 
 
-
-// var Member = ko.Model({
-// 	url : '/api/members',
-	
-
-// 	//init
-// 	//initialize 
-// 	// map : function(self, data){
-//  //        self.firstName = ko.observable(data.firstName)
-//  //            .extend({
-//  //                required: true,
-//  //                maxLength: 30
-//  //            });
-// 	// }
-
-// 	, showEmail : ko.computed(function(){
-//         return this.relationship() != 'Son' && 
-//                this.relationship() != 'Daughter' ;
-// 	})
-
-// 	, fields : {
-// 		firstName : { 
-// 			default : '', 
-// 			extend : {
-// 				required: true,
-// 				maxLength: 30
-// 			}
-// 		},
-// 		relationship : ''
-// 		lastName : '',
-// 		email : {
-// 			extend : {
-//                 email: {
-//                     message: 'please use valid email address'
-//                     , onlyIf : this.showEmail
-//                 },
-// 			}
-// 		}
-// 	},
-// })
-
 var Members = function() {
 	var self = this;
 
@@ -100,6 +59,10 @@ var Members = function() {
                            .then(self.selectedMember.clean);
     }
 
+    self.remove = function(member){
+        self.members.remove(member);
+    }
+
     self.edit = function(member){
     	self.selectedMember.fetch(member.id);
     }
@@ -107,89 +70,7 @@ var Members = function() {
 	self.activate = function(){
         self.members.fetch();
 	}
+
 	self.template = 'members-template';
 }
 
-
-
-        // self.relationship = ko.observable(data.relationship);
-
-        // self.firstName = ko.observable(data.firstName)
-        //     .extend({
-        //         required: true,
-        //         maxLength: 30
-        //     });
-        // self.lastName = ko.observable(data.lastName)
-        //     .extend({
-        //         required: true,
-        //         maxLength: 30
-        //     });
-
-        // self.day = ko.observable('');
-        // self.month = ko.observable('');
-        // self.year = ko.observable('');
-
-        // self.dateOfBirth = ko.observable(data.birthDate)
-        //     .extend({
-        //         date_parts: {
-        //             day: self.day,
-        //             month: self.month,
-        //             year: self.year
-        //         }
-        //     });
-        // self.address = ko.observable(data.address);
-        // self.telephoneNumber = ko.observable(data.tel);
-        // self.occupation = ko.observable(data.occupation);
-        // self.accountHolder = ko.observable(data.accountHolder);
-        // self.town = ko.observable(data.town);
-
-        // self.dateInvalid = ko.observable(false);
-
-        // self.showEmail = ko.computed(function(){
-        //     return self.relationship() != 'Son' && 
-        //            self.relationship() != 'Daughter' ;
-        // })
-
-        // self.email = ko.observable(data.email)
-        //     .extend({
-        //         required: {
-        //             onlyIf : self.showEmail
-        //         },
-        //         email: {
-        //             message: 'please use valid email address'
-        //             , onlyIf : self.showEmail
-        //         },
-        //         minLength: {
-        //             params : 2 
-        //             , onlyIf : self.showEmail
-        //         },
-        //         maxLength: {
-        //             params : 80 
-        //             , onlyIf : self.showEmail
-        //         },
-        //     });
-
-        // self.emailRepeat = ko.observable(data.email)
-        //     .extend({
-        //         required: {
-        //             onlyIf : self.showEmail
-        //         },
-        //         email: {
-        //             message: 'please use valid email address'
-        //             , onlyIf : self.showEmail
-        //         },
-        //         minLength: {
-        //             params : 2
-        //             , onlyIf : self.showEmail
-        //         },
-        //         maxLength: {
-        //             params : 80 
-        //             , onlyIf : self.showEmail
-        //         },
-        //         equal: {
-        //             params : self.email
-        //             , onlyIf : self.showEmail
-        //         }
-        //     });
-
-        // self.tempUrlMemberAvatar = ko.observable();
